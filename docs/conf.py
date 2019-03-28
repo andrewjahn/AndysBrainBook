f4f6535f421b00685200c688d4c0bb2347782f48
andrewjahn <48071253+andrewjahn@users.noreply.github.com>
2019-03-28 15:21:03 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'Andy\'s Brain Book'
+copyright = u'2019, Andy Jahn'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
