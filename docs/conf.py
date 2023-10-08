51a56a4777f9f6abfc784bd38bb050bdabe06924
Andrew Jahn <48071253+andrewjahn@users.noreply.github.com>
2023-10-07 22:48:14 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,6 @@
 import sys
 import os
 
-pip install sphinx_rtd_theme
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
