c372b5065822116ef9008d59a3d9deef2e51658f
Andrew Jahn <48071253+andrewjahn@users.noreply.github.com>
2025-05-15 20:12:36 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,9 @@ import os
 
 conda install -c conda-forge sphinx-copybutton
 
-extensions = ['sphinx_rtd_theme',
-             'sphinx_copybutton']
+extensions = ['sphinx_rtd_theme']
+
+# 'sphinx_copybutton'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
